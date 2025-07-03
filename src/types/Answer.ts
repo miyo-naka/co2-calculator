7a025f053c73cc7b627911fa2643cf531f0a163f
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2025-07-03 16:54:20 +1000

    added questions

---
+++ src/types/Answer.ts
@@ -0,0 +1,4 @@
+export type Answer = {
+  questionId: string;
+  selectedValue: string | string[];
+};
---
+++ src/utils/calculateCO2.ts
@@ -0,0 +1,34 @@
+import { Answer } from "@/types/Answer";
+import { Question } from "@/types/Question";
+
+export function calculateCO2(answers: Answer[], questions: Question[]): number {
+  let total = 0;
+
+  answers.forEach(({ questionId, selectedValue }) => {
+    const question = questions.find((q) => q.id === questionId);
+
+    if (question?.type === "input" && typeof selectedValue === "string") {
+      const num = parseFloat(selectedValue);
+      if (!isNaN(num) && question.factor) {
+        total += num * question.factor;
+      }
+    }
+
+    if (question?.type === "choice") {
+      const selectedOption = question.options?.find(
+        (opt) => opt.value === selectedValue
+      );
+      if (selectedOption?.co2Factor) total += selectedOption.co2Factor;
+      if (selectedOption?.score) total += selectedOption.score * 10;
+    }
+
+    if (question?.type === "multi" && Array.isArray(selectedValue)) {
+      selectedValue.forEach((val) => {
+        const opt = question.options?.find((o) => o.value === val);
+        if (opt?.score) total += opt.score * 10;
+      });
+    }
+  });
+
+  return total;
+}
